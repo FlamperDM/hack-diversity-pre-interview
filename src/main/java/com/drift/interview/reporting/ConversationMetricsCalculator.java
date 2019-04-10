2d9cf92d75878919a2a62a4b7f5d95272ddef669
Flampeyeiry Diaz <flampeyeiri12@gmail.com>
2019-04-10 10:47:17 -0400

    Completed challenge

--- src/main/java/com/drift/interview/reporting/ConversationMetricsCalculator.java
+++ src/main/java/com/drift/interview/reporting/ConversationMetricsCalculator.java
@@ -3,10 +3,12 @@ package com.drift.interview.reporting;
 import com.drift.interview.model.Conversation;
 import com.drift.interview.model.ConversationResponseMetric;
 import com.drift.interview.model.Message;
+
 import java.util.List;
 
 public class ConversationMetricsCalculator {
   public ConversationMetricsCalculator() {}
+
 
   /**
    * Returns a ConversationResponseMetric object which can be used to power data visualizations on the front end.
@@ -16,9 +18,38 @@ public class ConversationMetricsCalculator {
 
     // implement me!
 
+    long userFirstTime = 0;
+    long average = 0;
+    int countMsgAnswered = 0;
+
+    boolean firstMsg = true;
+
+    for (int i = 0; i < messages.size(); i++) {
+      Message msg = messages.get(i);
+      boolean isUser = !msg.isTeamMember();
+      if (!isUser) continue;
+
+      if (firstMsg && isUser)
+        userFirstTime = msg.getCreatedAt();
+
+      if (i + 1 < messages.size()){
+
+        if (isUser == !messages.get(i + 1).isTeamMember()){
+          firstMsg = false;
+        } else {
+          firstMsg = true;
+          countMsgAnswered++;
+          average += messages.get(i + 1).getCreatedAt() - userFirstTime;
+          i++;
+        }
+      }
+    }
+
+    average /= countMsgAnswered;
+
     return ConversationResponseMetric.builder()
         .setConversationId(conversation.getId())
-        .setAverageResponseMs(0)
+        .setAverageResponseMs(average)
         .build();
   }
 }
